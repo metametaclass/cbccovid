b17c2423d6c6388081b77f797f734ccbeac1c408
kate <none@none>
2020-07-14 12:57:37 +0300

    add result text

--- calculate.js
+++ calculate.js
@@ -194,8 +194,9 @@ $(document).ready(function(){
               findRange1("platelets", platelets, plateletsTable)+
               findRange1("monocytes", monocytes, monocytesTable)+
               findRange1("basophils", basophils, basophilsTable);
-
-     $("#result").text("Результат: "+points.toString()+" Вероятность COVID:"+findRange(points, probabilityTable)+"%");
+    var result = findRange(points, probabilityTable);
+    var resultText = (points>181)?" высокая, ": " низкая, ";
+     $("#result").text("Результат: "+points.toString()+", вероятность COVID-19 " + resultText + result + "%");
 
   });
 });
